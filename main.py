b38df0ea4faf066b3d9ca43a8e6b5691a3908310
Your Name <you@example.com>
2022-10-22 21:14:15 +1200

    работает

--- main.py
+++ main.py
@@ -1,22 +1,20 @@
+import asyncio
 import time
 import sys
 from fake_useragent import UserAgent
 from selenium import webdriver
+from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
 
+from bs4 import BeautifulSoup
 
-useragent = UserAgent()
 
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument('--headless')
-chrome_options.add_argument('--disable-gpu')
-chrome_options.add_argument(
-    "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36")
-chrome_options.add_argument("--disable-blink-features=AutomationControlled")
-chrome_options.add_argument("--disable-blink-features=AutomationControlled")
-if ("iPad" in chrome_options.arguments[4]) or ("Android" in chrome_options.arguments[4]):
-    chrome_options.arguments[:] = []
+# url = "https://www.ozon.ru/product/kvadrokopter-s-kameroy-radioupravlyaemyy-letayushchiy-dron-s-akkumulyatorom-724536456/?sh=h6B8DtGucQ"
+
+
+async def get_page(url: str) -> str:
+    useragent = UserAgent()
+
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--headless')
@@ -24,48 +22,74 @@ if ("iPad" in chrome_options.arguments[4]) or ("Android" in chrome_options.argum
     chrome_options.add_argument(
         "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36")
     chrome_options.add_argument("--disable-blink-features=AutomationControlled")
-    print(chrome_options.arguments[4])
+    chrome_options.add_argument("--disable-blink-features=AutomationControlled")
+    if ("iPad" in chrome_options.arguments[4]) or ("Android" in chrome_options.arguments[4]):
+        chrome_options.arguments[:] = []
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--disable-gpu')
+        chrome_options.add_argument(
+            "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36")
+        chrome_options.add_argument("--disable-blink-features=AutomationControlled")
+        print(chrome_options.arguments[4])
 
-print(chrome_options.arguments)
+    print(chrome_options.arguments)
 
-driver = webdriver.Chrome(executable_path="chromdrv/chromedriver", options=chrome_options)
+    driver = webdriver.Chrome(executable_path="chromdrv/chromedriver", options=chrome_options)
 
-url = "https://www.ozon.ru/product/ekshn-kamera-gopro-hero11-black-725415003/?advert=pQJnG6AZS5IeCyqDxnQIQB_doXOgvQzHGIlUj54Jc3TyeIaJTmnC-VW3mHvdXQBlpHrSutYmqGs45-EBFWH3C9LYKsGhWnaONMxp6mMZh3IvFkr14zW8EyRu72JkcVrxPpPbDa35o8LWZ9zOvdmm87RfaaSl6Iq5xZyQqbkzGhX6vspnpzO_9v0ZofuZ0_29afB3qF7FpmMoMqzCIEYR4H_Jp6dTpygMmnYXjS4VcN_Fs61y9dmORln576Hg6uerLY_Lf-C9A12LaTkhjEJOu3FdDVDl81LxqW28vB2J6FGGhi3NlSPSCwwWnZX_sRydj4cqR_yh4echVNxTYEEqF2BNRdVGaoYmSgYoxGtDGc7XUgJO9ph6jxETzVXei9orcvZp_zby5G2PsFdcfndwIDT-RnEDyYEpW6oTpuxZNIchJmKYAjsedyZnt3iAL04j2ol8KsTrmezHZnSkI08dOnXdzS57oZuUzX8wTeNnxrj4Pc2CG9_RKLnhchi15jEbCEy90CZBSx85rIqN4_DOdh92yes_p_X5EX7dv3MvD-jpkyr2dFTqgU7Bkp_5qff7Hjh_o8KDCaOWvqpPRiGhwod6zzcG6Ko0OJp_EoUAvAZTZmcROX7ViISEsR9EvrVUxCzS7HHviRTqUkJcLYV8-KgO1HezJLoDpU-8iG2bb9Y&avtc=1&avte=2&avts=1666419967&sh=h6B8DuOlnQ"
-
-def main():
     try:
-        pass
         driver.get(url=url)
+        html= driver.find_element(By.TAG_NAME, "html")
+        for i in range(2):
+            html.send_keys(Keys.PAGE_DOWN)
+            time.sleep(3)
+
         page_source = driver.page_source
-
-        with open("123.html", "w", encoding="utf-8") as file:
-            file.write(page_source)
-        # time.sleep(30)
-        #
-        # title = driver.find_element(By.XPATH, '//*[@id="layoutPage"]/div[1]/div[3]/div[2]/div/div/div[1]/div[2]/h1')
-        # print(title.text)
-        # # div_price = driver.find_element(By.XPATH, '//*[@id="layoutPage"]/div[1]/div[3]/div[3]/div[2]/div[2]/div/div/div/div[1]/div')
-        # current_price = driver.find_element(By.XPATH, '//*[@id="layoutPage"]/div[1]/div[3]/div[3]/div[2]/div[2]/div/div/div/div[1]/div/div/div/div/span[1]/span')
-        #
-        # prev_price = driver.find_element(By.XPATH, '//*[@id="layoutPage"]/div[1]/div[3]/div[3]/div[2]/div[2]/div/div/div/div[1]/div/div/div/div/span[2]')
-        #
-        # image = driver.find_element(By.XPATH, '//*[@id="layoutPage"]/div[1]/div[3]/div[3]/div[1]/div/div[2]').find_elements(By.TAG_NAME, "img")
-        #
-        # print(current_price.text)
-        # print(prev_price.text)
-        print(page_source)
-
-        #
-        # driver.get_screenshot_as_file("1.png")
+        return page_source
     except Exception as ex:
-        print(ex)
+        print(f'Не сработал Selenium - {ex}')
+        return ""
     finally:
         driver.close()
         driver.quit()
 
+async def get_product(url: str) -> dict:
+    product = {
+        "title": "",
+        "curr_price": "",
+        "prev_price": "",
+        "image_url": "",
+        "product_code": "",
+
+    }
+    page_source = await get_page(url)
+
+    try:
+        soup = BeautifulSoup(page_source, "html.parser")
+
+        title = soup.find("h1", class_="n9v")
+        curr_price = soup.find("span", class_="tn5 t5n").find("span")
+        prev_price = soup.find("span", class_="n6t")
+        image_url = soup.find("img", class_="m5l _24-a").get("src")
+        product_code = soup.find("span", class_="v9m wm")
+        description = soup.find("div", class_="ra-a1")
+        product["title"] = title.text
+        product["curr_price"] = curr_price.text
+        product["prev_price"] = prev_price.text
+        product["image_url"] = image_url
+        product["product_code"] = product_code.text
+        product["description"] = description.text
+    except Exception as e:
+        print(f'Не сработал BeautifulSoup - {e}')
+
+    print(product)
+    return product
+
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(get_product(
+        "https://www.ozon.ru/product/planshet-s-klaviaturoy-wuya-podderzhka-gps-navigatsii-podhodit-dlya-igr-kino-i-749429137/?advert=LO76fZ6RQAxLfIGlFx2TK6SLdOmyvvlR_gIG8g6c7pnEi9I2EhnpuVy2QWNQDElfRo3FAyCOr3R9nBj7ca_qKqAHR1kMQWo5fabX8KYQRgSMFW72btB8pNDRVKvys8Ud5cVZ76TgnNnuybJCmxNL_4k5x6DNkkkf4St2xOGK_-g0zE25O53v8DxcTvqESnLEOGFDOqJ_cGlBxhOGN7a7cA7g-XofdZiiXocQMVjqgyV0AIGmnEOrKqn9kJda-wLh9a3kqDpYpKRYL9kCiNPcJrRLLaIBI06lAjSeCco3XdNpbNhNP0xubZ2KtMNhNkLGzyn2-vOeEPUaBE7DAn7AXrC_Q12umW-QcmtKqnH9BLq9gA2PCWj1SzwLcKA0tuaQMr4_FD1w-w1Y9ljBXts_YgyHwtWVxFa_k_0VRZIpB3JjQTpb6FJQvrTfVXc33stWehYBGzlDU0c-fwBkPTS-LAJJ0STBDbEyz22Q5CtKxBdsj4Ou_SaJdbUIAeUkjB6Jotk-dHT_ofwIfLHmT3jVrNIJkQFr7rMXBPWIlHCpGBxejDQ3-tVBWk6xS-3WOOU9AIOPE3E8oB0ldXZs7OZXoCQ-rKCwuzVKWl8_7oxcj-kfchi5GpmW7rUlBuKzvbOOWlQ-ziV6oO-t6O1g3YIiTs-TUj5ZpQpOHMqn6Yemb9c&avtc=1&avte=2&avts=1666429096&sh=h6B8Dgt9jw"))
 
 # url = "https://www.ozon.ru/api/composer-api.bx/page/json/v2?url=/product/stiralnaya-mashina-atlant-sma-60s1010-00-belyy-257776483"
 #
